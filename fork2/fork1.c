d64403674897c2074d033e424e06704e163dbf24
LuisAngelCv <114983886+LuisAngelcv02@users.noreply.github.com>
2023-10-04 13:43:19 +0200

    fork1.c

--- fork2/fork1.c
+++ fork2/fork1.c
@@ -5,48 +5,73 @@
 #include <sys/wait.h>
 
 void main(){
-    pid_t pid1,pid2,pid3,pid4;
+    pid_t pid1,pid2,pid3;
     int status;
-
+    pid4=getpid();
     pid1=fork();
 
     if (pid1==0)
     {
-        pid3=fork();
-
-        if (pid3==0)
-        {
-            pid4=fork();
-
-            if(pid4==0){
-            printf("Proceso p4 %d\n",getpid());
-                
-            }
-            
-            else{
-            waitpid(pid4,&status,0);
-            printf("Proceso p3 %d\n",getpid());
-            }
+        if(getpid()%2==0){
+        printf("p2 %d\n",getpid());
+        printf("padre p2 es p1: %d\n",getppid());
+        }
+        else{
+            printf("p2 %d\n",getpid());
         }
         
     }
 
     else{
-        //pid3=wait(NULL);
-        //pid1=wait(NULL);
-       
-   //waitpid(pid1,&status,0);
-   waitpid(pid3,&status,0);
-   pid2=fork();
-        if(pid2==0){
-            printf("Proceso p2 %d\n",getpid());
+        
+       // printf("p1 %d\n",getpid());
+        //printf("hijo de p1 es p2 %d\n",pid1)
+        
+        pid2=fork();
+        if(pid2==0)
+        {
+            pid3=fork();
+            if(pid3==0){
+                if(getpid()%2==0){
+                    printf("proceso p4 %d\n",getpid());
+                    printf("padre p4 es p3 %d\n",getppid());
+                }
+                else{
+                    printf("proceso p4 %d\n",getpid());
+                }
+                
+            }
+            else{
+                //pid3=wait(NULL);
+                waitpid(pid3,&status,0);
+                if(pid3%2!=0){
+                printf("proceso p3 %d\n",getpid());
+                printf("proceso padre p3 es p2: %d\n",getppid());
+                
+                }
+                else{
+                    printf("proceso p3 %d\n",getpid());
+            }
+            
+        }
         }
         else{
-            waitpid(pid2,&status,0);
+            pid1=wait(NULL);
+            pid3=wait(NULL);
+            //waitpid(pid1,&status,0);
+            //waitpid(pid2,&status,0);
             
-            printf("Proceso p1 %d\n",getpid());
+            if(getpid()%2==0){
+                printf("p1%d\n",getpid());
+                printf("p1 padre es %d\n",getppid());
+            }
+            else{
+                printf("p1%d\n",getpid());
+            }
+
         }
-    
+        
+        
     }
     
 }
